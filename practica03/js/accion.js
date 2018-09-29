9823ac5a33ce0a817b2f121e13eb1f464c2b021a
powervic <33923406+powervic@users.noreply.github.com>
2018-09-29 14:11:49 +0200

    Pracitca 03 mejorada

--- practica03/js/accion.js
+++ practica03/js/accion.js
@@ -6,16 +6,43 @@ $(function() {
         var conexion = $.ajax({
             url: 'https://randomuser.me/api/?results=100',
             dataType: 'json',
-            success: function(data) {
-            }
+            success: function(data) {}
         });
-        conexion.done(function(data){
-        	for (persona of data["results"]){
-        		$("#listado").append("<p>"+"<img src="+persona["picture"]["large"]+" "+"</img>"+persona["name"]["first"]+" "+persona["name"]["last"]+"<br>"+
-        			persona["email"]+"<br>"+persona["location"]["street"]+", "+persona["location"]["postcode"]+", "
-        			+persona["location"]["city"]+" ("+persona["location"]["state"]+")"+"</p>")
-        	}
-        })
+        conexion.done(function(data) {
+            var i = 0;
+            for (persona of data["results"]) {
+                $("#listado").append("<div class=\"fila\" id=\"" + i + "\">" + "<figure><img src=" + persona["picture"]["large"] + "></img></figure>" +
+                    "<p>" + persona["name"]["first"] + " " +
+                    persona["name"]["last"] + "<br>" +
+                    persona["email"] + "<br>" +
+                    persona["location"]["street"] + ", " +
+                    persona["location"]["postcode"] + ", " +
+                    persona["location"]["city"] + " (" +
+                    persona["location"]["state"] + ")" + "</p></div>")
+                i++;
+            };
+        });
 
+    });
+    $('body').on('click', '.fila', function(e) {
+        var id = $(this).attr('id');
+        var conexion2 = $.ajax({
+            url: 'https://randomuser.me/api/?results=1',
+            dataType: 'json',
+            success: function(data) {}
+        });
+
+        conexion2.done(function(data) {
+            var pers = data["results"][0];
+            
+            $('#' + id).html("<figure><img src=" + pers["picture"]["large"] + "></img></figure>" +
+                "<p>" + pers["name"]["first"] + " " +
+                pers["name"]["last"] + "<br>" +
+                pers["email"] + "<br>" +
+                pers["location"]["street"] + ", " +
+                pers["location"]["postcode"] + ", " +
+                pers["location"]["city"] + " (" +
+                pers["location"]["state"] + ")" + "</p>");
+        });
     });
 });
